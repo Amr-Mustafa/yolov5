8a257f95d02a54102f4275f588780a343a59d64f
Amr Mustafa <amr.mustafa.97@outlook.com>
2022-04-05 19:56:26 +0200

    Update detect.py

--- detect.py
+++ detect.py
@@ -85,7 +85,7 @@ def run(
         source = check_file(source)  # download
 
     # Directories
-    save_dir = increment_path(Path(project) / name, exist_ok=exist_ok)  # increment run
+    save_dir = Path(project) / name#increment_path(Path(project) / name, exist_ok=exist_ok)  # increment run
     (save_dir / 'labels' if save_txt else save_dir).mkdir(parents=True, exist_ok=True)  # make dir
 
     # Load model
